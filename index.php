4bc7ad3e406052a7f8557901edfcaa4bfd4089cc
Peter Karman <karpet@peknet.com>
2020-10-15 17:24:05 -0500

    add google tags

--- index.php
+++ index.php
@@ -58,6 +58,18 @@
     <!-- icons -->
     <script src="https://kit.fontawesome.com/858b4d9129.js" crossorigin="anonymous" SameSite="none Secure"></script>
 
+    <?php if ($_ENV['GOOGLE_TAG_MANAGER']) { ?>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=<?= $_ENV['GOOGLE_TAG_MANAGER'] ?>"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '<?= $_ENV['GOOGLE_TAG_MANAGER'] ?>');
+    </script>
+    <?php } ?>
+
     <title>Kansas <?= $_ENV['ELECTION_YEAR'] || '2020' ?> Voter Guide</title>
 
     <style>
