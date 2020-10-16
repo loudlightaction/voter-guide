6cd088a13bd8b94d2663093de7649ccba68168ea
Peter Karman <karpet@peknet.com>
2020-10-16 16:26:55 -0500

    revert answer spacing

--- candidates-layout.php
+++ candidates-layout.php
@@ -52,14 +52,14 @@
   </div>
   <?php foreach($candidates as $candidate) { ?>
   <div class="row mb-2 candidate align-items-center">
-    <div class="col-3">
+    <div class="col-2">
       <?php if (isset($candidate->{'fields'}->{'Photo'}[0]->{'thumbnails'}->{'small'}->{'url'})) { ?>
       <img class="img-thumbnail <?= $candidate->{'fields'}->{'Party'} ?>" src="<?= $candidate->{'fields'}->{'Photo'}[0]->{'thumbnails'}->{'small'}->{'url'} ?>" />
       <?php } else { ?>
       <i class="far fa-address-card fa-2x <?= $candidate->{'fields'}->{'Party'} ?>"></i>
       <?php } ?>
     </div>
-    <div class="col-3">
+    <div class="col">
       <?= $candidate->{'fields'}->{'last name'} ?>
     </div>
     <div class="col">
