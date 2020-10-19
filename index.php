c5f5dbdb60d4c7f0bf3f0fd8cf9197e8fe770a49
Peter Karman <karpet@peknet.com>
2020-10-18 22:12:17 -0500

    footer link

--- index.php
+++ index.php
@@ -260,7 +260,7 @@
           <div><a class="text-light" href="https://www.ksvotes.org/r/ksvoterguide">Voting Location</a></div>
           <div><a class="text-light" href="https://www.ksvotes.org/r/ksvoterguide">Check Voter Registration</a></div>
           <div><a class="text-light" href="https://myvoteinfo.voteks.org/voterview/">Sample Ballot</a></div>
-          <div><a class="text-light" href="#">Issue Stances Details</a></div>
+          <div><a class="text-light" href="https://loudlightaction.org/methodology">Issue Stance Methodology</a></div>
           <div class="font-italic"><a class="text-light" href="https://loudlightaction.org/">A project of Loud Light Civic Action</a></div>
         </div>
       </div>
